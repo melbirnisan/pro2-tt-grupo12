ef8e834c33fea5f5caa34f7d7edfdf0d5cb6ecb3
melinebirnisan <mbirnisan@udesa.edu.ar>
2024-06-20 23:39:03 -0300

    termine logout y agregar producto

--- tpIntegrador/controllers/productController.js
+++ tpIntegrador/controllers/productController.js
@@ -1,5 +1,6 @@
 const datos = require("../database/models" );
 const op = datos.Sequelize.Op
+const{validationResult} = require("express-validator")
 
 const productController = {
     index: function(req,res){
@@ -24,8 +25,34 @@ const productController = {
         });
     },
 
+    store: function (req, res) {
+        let form = req.body;
+        let errors = validationResult(req); 
+
+        if (errors.isEmpty()){
+            datos.Producto.create(form)
+            .then(function (results) {
+                return res.redirect('/product/id/' + results.id);
+            })
+            .catch(function (error) {
+                return console.log(error);
+            })
+        } else {
+            return res.render("product-add", {
+              errors: errors.mapped(),
+              old: req.body
+          })
+          };
+        
+    },
+
     add: function(req,res){
-        return res.render('product-add', {profile : datos.usuario})
+        if (req.session.user != undefined) {
+            return res.render('product-add', {profile : datos.usuario})
+        }
+        else {
+            return res.redirect("/users/login");
+        }
     },
 };
 
--- tpIntegrador/routes/products.js
+++ tpIntegrador/routes/products.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const productController = require('../controllers/productController')
+const productController = require('../controllers/productController');
+const { body } = require('express-validator');
+const validations = [
+    body("nombreProducto")
+        .notEmpty().withMessage("El campo nombre es obligatorio, ingresa un nombre").bail(),
+    body("descripcion")
+        .notEmpty().withMessage("El campo descrpcion es obligatorio, ingresa una descripcion").bail(),
+    body("fotoProducto")
+        .notEmpty().withMessage("El campo imagen es obligatorio, ingresa una imagen").bail(),
+
+
+]
 
 router.get("/id/:id", productController.index);
 
 router.get("/add", productController.add);
+
+router.post("/add", validations, productController.store)
 
 module.exports = router; 
